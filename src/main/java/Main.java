2fcd29ddb6d97d9fd765afa6b09e4aeb237eeb5d
Saka1711 <saikarthik.neriyanuri@gmail.com>
2025-03-16 16:03:17 +0530

    Create Blob

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,13 @@
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.util.zip.DeflaterOutputStream;
 import java.util.zip.InflaterInputStream;
 
 public class Main {
@@ -25,6 +30,18 @@ public class Main {
         }
         String blobSha = args[2];
         readBlob(blobSha);
+      }
+      case "hash-object" -> {
+        if(args.length != 3 || !args[1].equals("-w")){
+          System.out.println("Usage: hash-object -w <file>");
+          return;
+        }
+        try {
+          String sha1Hash = createGitBlob(args[2]);
+          System.out.println(sha1Hash);
+        } catch (Exception e) {
+          throw new RuntimeException("Error creating Git Blob", e);
+        }
       }
       default -> System.out.println("Unknown command: " + command);
     }
@@ -64,5 +81,41 @@ public class Main {
     } catch (IOException e) {
       throw new RuntimeException("Error Reading Blob File", e);
     }
+  }
+
+  private static String createGitBlob(String filePath) throws Exception{
+    byte[] content = Files.readAllBytes(Paths.get(filePath));
+    String header = "blob " + content.length + "\0";
+    byte[] blob = concatenate(header.getBytes(), content);
+    
+    MessageDigest md = MessageDigest.getInstance("SHA-1");
+    byte[] sha1Bytes = md.digest(blob);
+    String sha1Hash = bytesToHex(sha1Bytes);
+
+    String dir = ".git/objects/" + sha1Hash.substring(0, 2);
+    String file = sha1Hash.substring(2);
+
+    Files.createDirectories(Paths.get(dir));
+    try(OutputStream os = new FileOutputStream(dir + "/" + file);
+        DeflaterOutputStream dos = new DeflaterOutputStream(os)
+    ){
+      dos.write(blob);
+    }
+    return sha1Hash;
+  }
+
+  private static byte[] concatenate(byte[] a, byte[] b) {
+    byte[] result = new byte[a.length + b.length];
+    System.arraycopy(a, 0, result, 0, a.length);
+    System.arraycopy(b, 0, result, a.length, b.length);
+    return result;
+  }
+
+  private static String bytesToHex(byte[] bytes) {
+    StringBuilder sb = new StringBuilder();
+    for (byte b : bytes) {
+      sb.append(String.format("%02x", b));
+    }
+    return sb.toString();
   }
 }
